361ea9c73dd02cd4e540169f979cec501b68311e
Brian-loop <brianloop.github@gmail.com>
2025-09-30 00:38:40 -0300

    estrutura do cadastro de pets

---
+++ cad_pets.php
@@ -0,0 +1,43 @@
+<?php
+include './template/header.php';
+?>
+
+<section class="page_cad_pets">
+    <main class="cad_pets">
+        <nav class="cad_pets_itens1">
+
+        </nav>
+        <nav class="cad_pets_itens2">
+            <div class="group_inputs_pet">
+                <div>
+                    <label for="">nome</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Espécie</label>
+                    <input type="text">
+                    <label for="">Sexo</label>
+                    <input type="text">
+                    <label for="">idade</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Raça</label>
+                    <input type="text">
+                    <label for="">Porte</label>
+                    <input type="text">
+                </div>
+                <div>    
+                    <label for="">Castrado</label>
+                    <input type="text">
+                    <label for="">Vacinado</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Motivo da doação</label>
+                    <textarea name="" id="" rows="5" cols="33"></textarea>
+                </div>
+            </div>
+        </nav>
+    </main>    
+</section>
