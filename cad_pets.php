d8f3d8d739578f86f8d6d308501aedc605785e00
Brian-loop <brianloop.github@gmail.com>
2025-09-30 23:53:42 -0300

    estrutura de cadastro de pets

--- cad_pets.php
+++ cad_pets.php
@@ -10,32 +10,64 @@ include './template/header.php';
         <nav class="cad_pets_itens2">
             <div class="group_inputs_pet">
                 <div>
-                    <label for="">nome</label>
+                    <label for="">Nome do pet</label>
                     <input type="text">
+                    <label for="">Sexo</label>
+                    <select name="" id="">
+                        <option value="" disabled selected>--Selecione--</option>
+                        <option value="">Macho</option>
+                        <option value="">Fêmea</option>
+                    </select> 
+                    <!-- <select id="idade" name="idade">
+  <option value="">Selecione a idade</option>
+  <option value="0-1">0-1 ano</option>
+  <option value="2-5">2-5 anos</option>
+  <option value="6-10">6-10 anos</option>
+  <option value="mais-10">Mais de 10 anos</option>
+</select> -->
                 </div>
                 <div>
                     <label for="">Espécie</label>
-                    <input type="text">
-                    <label for="">Sexo</label>
-                    <input type="text">
+                    <input type="text">                 
                     <label for="">idade</label>
-                    <input type="text">
+                    <input type="number" style="width: 3rem;" pattern="[0-9]{2}" maxlength="99" required>
+                    <select name="" id="">
+                        <option value="">--Selecione--</option>
+                        <option value="">Semanas</option>
+                        <option value="">Messes</option>
+                        <option value="">Anos</option>
+                    </select>
                 </div>
                 <div>
                     <label for="">Raça</label>
                     <input type="text">
                     <label for="">Porte</label>
-                    <input type="text">
+                    <select name="" id="">
+                        <option value="" disabled selected>--Selecione--</option>
+                        <option value="">Grande</option>
+                        <option value="">Medio</option>
+                        <option value="">Pequeno</option>
+                    </select>
                 </div>
                 <div>    
                     <label for="">Castrado</label>
-                    <input type="text">
+                    <select name="" id="">
+                        <option value="" disabled selected>--Selecione--</option>
+                        <option value="">Sim</option>
+                        <option value="">Não</option>
+                    </select>
+
+
                     <label for="">Vacinado</label>
-                    <input type="text">
+                     <select name="" id="">
+                        <option value="" disabled selected>--Selecione--</option>
+                        <option value="">Sim</option>
+                        <option value="">Não</option>
+                    </select>
                 </div>
                 <div>
                     <label for="">Motivo da doação</label>
-                    <textarea name="" id="" rows="5" cols="33"></textarea>
+                    <textarea name="" id="" rows="5" cols="33" style=" resize: none;"></textarea>
                 </div>
             </div>
         </nav>
