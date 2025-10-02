e3ac40eb880ccbe370059be6a75589d08e17e3e7
Brian-loop <brianloop.github@gmail.com>
2025-10-02 00:32:10 -0300

    estrutura do cadastro de usuario

---
+++ tela_cad_entrar_usuarios.php
@@ -0,0 +1,15 @@
+<?php 
+include './template/header.php';
+?>
+<section class="tela_cad_entrar">
+    <main class="group_usuarios_inputs">
+        <nav class="entrar_usuario">
+            <label for="entra">E-mail</label>
+            <input type="text">
+        </nav>
+        <nav class="cad_usuario">
+
+        </nav>
+
+    </main>
+</section>
