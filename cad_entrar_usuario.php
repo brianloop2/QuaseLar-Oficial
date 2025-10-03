9e15da4aa7c4d6cc74b1b9c6d1b1572904017f05
brianloop2 <brianloop.github@gmail.com>
2025-10-03 10:37:32 -0300

    inicio de estruturas

---
+++ cad_entrar_usuario.php
@@ -0,0 +1,7 @@
+<?php 
+//pegue as informações dos inputs da " tela_cad_entrar_usuarios.php "
+//este espaço é par realiazar os cadastros dos usuarios, 
+
+//*ainda n criado
+// include 'CLASSE DO banco';
+
