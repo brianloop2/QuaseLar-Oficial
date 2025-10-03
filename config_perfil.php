6b0d09f3c5d1d1dde3c5d09d3faa99808970c246
brianloop2 <brianloop.github@gmail.com>
2025-10-03 10:51:35 -0300

    configurações de usuario

---
+++ config_perfil.php
@@ -0,0 +1,6 @@
+<?php
+//pegue as informações dos inputs da " tela_config_perfil.php "
+//este espaço é par realiazar as sessoes de configuarções de usuario, 
+
+//*ainda n criado
+// include 'CLASSE DO banco';
---
+++ tela_config_perfil.php
@@ -0,0 +1,2 @@
+<?php
+//tela de perfil
