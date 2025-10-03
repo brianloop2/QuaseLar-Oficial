5b316243e4e6f9c2b13c9954adc0c900ba9c40b0
brianloop2 <brianloop.github@gmail.com>
2025-10-03 10:50:51 -0300

    estrutura de cadastro de pets desaparecidos/inclusao de cards

---
+++ cad_procurados.php
@@ -0,0 +1,6 @@
+<?php
+//pegue as informações dos inputs da " tela_cad_procurados.php "
+//este espaço é par realiazar cadastro de pets desaparecidos, 
+
+//*ainda n criado
+// include 'CLASSE DO banco';
---
+++ tela_cad_procurados.php
@@ -0,0 +1,3 @@
+<?php
+include './template/header.php';
+//tela de cadastro de pet desaparecidos
